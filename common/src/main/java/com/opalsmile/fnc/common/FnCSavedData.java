7fd74d6974a3f2c20be6a0164da3ccb0f6b7080a
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2023-08-04 00:29:26 +0200

    Add custom saved data

---
+++ common/src/main/java/com/opalsmile/fnc/common/FnCSavedData.java
@@ -0,0 +1,83 @@
+package com.opalsmile.fnc.common;
+
+import com.opalsmile.fnc.FnCConstants;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.saveddata.SavedData;
+
+import java.util.UUID;
+
+//Only attached to the overworld in theory.
+public class FnCSavedData extends SavedData {
+
+    //Used to keep track of the one jockey instance per overworld
+
+    private BlockPos spawnPosition;
+    private UUID jockeyUUID;
+
+    private long jockeyCooldown;
+
+    private boolean jockeySpawned;
+
+    public static final String FNC_SAVED_CLAIM_DATA_ID = FnCConstants.MOD_ID + "_fnc_saved_claim_data";
+
+    public static FnCSavedData get(ServerLevel level) {
+        return level.getDataStorage().computeIfAbsent(FnCSavedData::load, FnCSavedData::new, FNC_SAVED_CLAIM_DATA_ID);
+    }
+
+    public static FnCSavedData load(final CompoundTag tag) {
+        FnCSavedData savedData = new FnCSavedData();
+        if (tag.getBoolean("jockey_spawned")) {
+            savedData.jockeySpawned = true;
+            savedData.jockeyUUID = tag.getUUID("jockey_uuid");
+            savedData.spawnPosition = NbtUtils.readBlockPos(tag.getCompound("jockey_position"));
+        }
+        savedData.jockeyCooldown = tag.getLong("jockey_cooldown");
+        return savedData;
+    }
+
+    @Override
+    public CompoundTag save(CompoundTag compoundTag){
+        compoundTag.putBoolean("jockey_spawned", this.jockeySpawned);
+        if (jockeySpawned) {
+            compoundTag.putUUID("jockey_uuid", jockeyUUID);
+            compoundTag.put("jockey_position", NbtUtils.writeBlockPos(spawnPosition));
+        }
+        compoundTag.putLong("jockey_cooldown", jockeyCooldown);
+        return compoundTag;
+    }
+
+    public BlockPos getSpawnPosition(){
+        return spawnPosition;
+    }
+
+    public void setSpawnPosition(BlockPos spawnPosition){
+        this.spawnPosition = spawnPosition;
+    }
+
+    public UUID getJockeyUUID(){
+        return jockeyUUID;
+    }
+
+    public void setJockeyUUID(UUID jockeyUUID){
+        this.jockeyUUID = jockeyUUID;
+    }
+
+    public boolean hasJockeySpawned() {
+        return jockeySpawned;
+    }
+
+    public void setJockeySpawned(boolean jockeySpawned){
+        this.jockeySpawned = jockeySpawned;
+    }
+
+    public long getJockeyCooldown(){
+        return jockeyCooldown;
+    }
+
+    public void setJockeyCooldown(long jockeyCooldown){
+        this.jockeyCooldown = jockeyCooldown;
+    }
+}
