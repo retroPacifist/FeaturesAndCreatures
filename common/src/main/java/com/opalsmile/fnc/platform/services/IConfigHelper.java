7619d685adac3aa0f6b0c1e67b70a82e4693260a
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2023-08-04 17:21:43 +0200

    Improve documentation of configs

--- common/src/main/java/com/opalsmile/fnc/platform/services/IConfigHelper.java
+++ common/src/main/java/com/opalsmile/fnc/platform/services/IConfigHelper.java
@@ -6,14 +6,42 @@ import java.util.Set;
 
 public interface IConfigHelper {
 
+    int JOCKEY_DEFAULT_COOLDOWN = 3 * 20 * 60 * 20;
+    int JOCKEY_DEFAULT_ALIVE_TIME = 3 * 20 * 60 * 20;
+
+    /**
+     * Jockey spawn chance. Clamped between 0 and 1. Default value is 0.5
+     * @return A double representing the chance for the jockey to spawn
+     */
     double getJockeySpawnChance();
+
+    /**
+     * Whether named jockeys should still despawn.
+     * @return Whether named jockeys should despawn.
+     */
 
     boolean namedJockeyDespawn();
 
     /**
      * Serialized as a List<String> mapped with ResourceLocation::new and Registry#get
      *
-     * @return
+     * @return A list of mob effects that are blacklisted from appearing in the Jockey's shop
      */
     Set<MobEffect> jockeyEffectBlacklist();
+
+    /**
+     * The amount of in game time it takes the Jockey to despawn when completely loaded.
+     * The default amount is 3 in game days (3 * 20 * 60 * 20), since a day is 20 mins
+     * Clamped between 0 and max integer.
+     * @return The amount of time a jockey is around for
+     */
+    int jockeyDespawnTime();
+
+    /**
+     * The amount of time the game takes between attempting to even spawn a jockey.
+     * Resets whenever there is a failed attempt.
+     * Clamped between 0 and max integer.
+     * @return The amount of time the game waits before attempting to spawn a jockey.
+     */
+    int jockeySpawningCooldown();
 }
--- fabric/src/main/java/com/opalsmile/fnc/platform/FabricConfigHelper.java
+++ fabric/src/main/java/com/opalsmile/fnc/platform/FabricConfigHelper.java
@@ -7,18 +7,56 @@ import java.util.Set;
 
 public class FabricConfigHelper implements IConfigHelper {
 
+    /**
+     * Jockey spawn chance. Clamped between 0 and 1. Default value is 0.5
+     * @return A double representing the chance for the jockey to spawn
+     */
     @Override
     public double getJockeySpawnChance(){
-        return 50;
+        return 0.5;
     }
 
+    /**
+     * Whether named jockeys should still despawn.
+     *
+     * @return Whether named jockeys should despawn.
+     */
     @Override
     public boolean namedJockeyDespawn(){
         return false;
     }
 
+    /**
+     * Serialized as a List<String> mapped with ResourceLocation::new and Registry#get
+     *
+     * @return A list of mob effects that are blacklisted from appearing in the Jockey's shop
+     */
     @Override
     public Set<MobEffect> jockeyEffectBlacklist(){
         return Set.of();
+    }
+
+    /**
+     * The amount of in game time it takes the Jockey to despawn when completely loaded.
+     * The default amount is 3 in game days (3 * 20 * 60 * 20), since a day is 20 mins
+     * Clamped between 0 and max integer.
+     *
+     * @return The amount of time a jockey is around for
+     */
+    @Override
+    public int jockeyDespawnTime(){
+        return JOCKEY_DEFAULT_ALIVE_TIME;
+    }
+
+    /**
+     * The amount of time the game takes between attempting to even spawn a jockey.
+     * Resets whenever there is a failed attempt.
+     * Clamped between 0 and max integer.
+     *
+     * @return The amount of time the game waits before attempting to spawn a jockey.
+     */
+    @Override
+    public int jockeySpawningCooldown(){
+        return JOCKEY_DEFAULT_COOLDOWN;
     }
 }
