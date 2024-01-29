b86efb20fc0da6a77579601b26d17e8c03e5b493
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2024-01-29 17:48:21 +0100

    Remove the Antler Headdress's tooltip

--- common/src/main/java/com/opalsmile/fnc/item/AntlerHeaddress.java
+++ common/src/main/java/com/opalsmile/fnc/item/AntlerHeaddress.java
@@ -1,5 +1,10 @@
 package com.opalsmile.fnc.item;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import software.bernie.geckolib.animatable.GeoItem;
@@ -15,11 +20,17 @@ public abstract class AntlerHeaddress extends ArmorItem implements GeoItem {
         super(material, type, itemProperties);
     }
 
+
+    //No attributes, no armor
+    @Override
+    public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot $$0){
+        return ImmutableMultimap.of();
+    }
+
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers){
 
     }
-
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache(){
         return cache;
