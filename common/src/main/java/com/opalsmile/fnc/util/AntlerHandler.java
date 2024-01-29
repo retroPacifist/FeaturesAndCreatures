f598be42a6933b752e6fd9e9607ae68316533eae
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2024-01-29 14:52:53 +0100

    Make chaaarge only be given when hitting an entity

--- common/src/main/java/com/opalsmile/fnc/util/AntlerHandler.java
+++ common/src/main/java/com/opalsmile/fnc/util/AntlerHandler.java
@@ -76,11 +76,11 @@ public class AntlerHandler {
         player.setDeltaMovement(player.getDeltaMovement().add(xForwards, 0.0, zForwards));
         AABB targetBoundingBox = player.getBoundingBox().expandTowards(xForwards, 0, zForwards);
         level.getEntities(EntityTypeTest.forClass(Entity.class), targetBoundingBox, entity -> entity.isAlive() && entity != player).forEach(entity -> {
+            FnCTriggers.ANTLER_HEADDRESS.trigger(player, player.getItemBySlot(EquipmentSlot.HEAD));
             entity.move(MoverType.PLAYER, new Vec3(knockback * vec.x, 2.0, knockback * vec.z));
             entity.hurt(level.damageSources().mobAttack(player), MAX_DAMAGE * chargePercentage);
             level.playSound(null, entity.getX(), entity.getY(), entity.getZ(), FnCSounds.ANTLER_HEADDRESS_ATTACK_STRONG.get(), SoundSource.PLAYERS, 1.0f, 1.0f);
         });
-        FnCTriggers.ANTLER_HEADDRESS.trigger(player, player.getItemBySlot(EquipmentSlot.HEAD));
         player.hurtMarked = true;
     }
 
